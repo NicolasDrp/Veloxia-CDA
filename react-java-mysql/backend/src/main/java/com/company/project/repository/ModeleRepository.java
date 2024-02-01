37d2114823e08ed295ee8577ad0194bef19a2c12
WazeAbi <alexdalle12@gmail.com>
2024-02-01 12:30:01 +0100

    Feat(Service) : Creation Modele Service

--- react-java-mysql/backend/src/main/java/com/company/project/repository/ModeleRepository.java
+++ react-java-mysql/backend/src/main/java/com/company/project/repository/ModeleRepository.java
@@ -4,6 +4,9 @@ import com.company.project.entity.Modele;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ModeleRepository extends CrudRepository<Modele, Integer> {
+    List<Modele> findAll();
 }
