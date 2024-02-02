754f039f016fdc5d6c92d1715d012503e95c5801
NicolasDrp <drapier.nicolas07@gmail.com>
2024-02-02 19:31:46 +0100

    fix(front) : header en fixe

--- react-java-mysql/frontend/src/Composant/Header.tsx
+++ react-java-mysql/frontend/src/Composant/Header.tsx
@@ -4,7 +4,7 @@ import Logo from '../image/veloxia.png';
 
 const Header = () => {
     return (
-        <header className={"flex justify-between items-center pl-10 pr-10"}>
+        <header className={"flex justify-between items-center pl-10 pr-10 fixed w-full z-50"}>
             <img src={Logo} alt=""/>
             <button
                 className="bg-gray-900 hover:bg-gray-950 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
