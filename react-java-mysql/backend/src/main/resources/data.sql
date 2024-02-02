e7428642723a4a954c858a96d8c92c7ea99c09d6
NicolasDrp <drapier.nicolas07@gmail.com>
2024-02-02 20:15:13 +0100

    feat(bdd) : ajout modeles data.sql

--- react-java-mysql/backend/src/main/resources/data.sql
+++ react-java-mysql/backend/src/main/resources/data.sql
@@ -1,2 +1,10 @@
+--Ajout d'un utilisateur
 INSERT INTO utilisateur (prenom_utilisateur, nom_utilisateur, mot_de_passe, email_utilisateur)
 VALUES ('prenom', 'nom', 'motdepasse123', 'mail@mail.com');
+
+-- ajoute de modele de voiture
+INSERT INTO modele (nom_modele, portes, prix_modele) VALUES ('KLASIC', 4, 30000);
+
+INSERT INTO modele (nom_modele, portes, prix_modele) VALUES ('LOTRE', 5, 40000);
+
+INSERT INTO modele (nom_modele, portes, prix_modele) VALUES ('ENKOR1OTRE', 2, 25000);
