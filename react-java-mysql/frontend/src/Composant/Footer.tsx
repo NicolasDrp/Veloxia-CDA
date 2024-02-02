2eb7c2b07b5f549512aabf42680e1461df8296a4
NicolasDrp <drapier.nicolas07@gmail.com>
2024-02-02 23:00:20 +0100

    feat(front) : couleur footer change selon la page

--- react-java-mysql/frontend/src/Composant/Footer.tsx
+++ react-java-mysql/frontend/src/Composant/Footer.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Footer: React.FC = () => {
+  const location = useLocation();
+  const isAccueilPage = location.pathname === '/';
+
   return (
-    <footer className="text-center py-4 fixed bottom-0 w-full z-50">
-      <p className="text-white text-xl">©️Veloxia 2024  Mentions légales </p>
+    <footer className={`text-center py-4 fixed bottom-0 w-full z-50 ${isAccueilPage ? 'text-white' : 'text-black'}`}>
+      <p className="text-xl">©️Veloxia 2024  Mentions légales </p>
     </footer>
   );
 };
