5dfe4465123f73ac8c41c01328025684a7bf4ae1
NicolasDrp <drapier.nicolas07@gmail.com>
2024-02-02 19:15:55 +0100

    feat(front) : page connexion

--- react-java-mysql/frontend/src/Pages/Connexion.tsx
+++ react-java-mysql/frontend/src/Pages/Connexion.tsx
@@ -2,26 +2,26 @@ import React from 'react';
 
 const Connexion = () => {
     return (
-        <div className="w-full max-w-xl m-auto">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <h2 className="" >Se connecter</h2>
-                <div className="mb-10">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+        <div className="flex items-center justify-center mt-40">
+            <form className="bg-white rounded px-24 pt-6 pb-8 mb-4 w-1/3 h-1/2">
+                <h2 className="text-center font-bold text-2xl mb-10">Se connecter</h2>
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-lg font-bold mb-2" htmlFor="email">
                         Email
                     </label>
-                    <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email" />
+                    <input className="appearance-none border rounded w-full py-3 px-4 text-lg text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email" />
                 </div>
-                <div className="mb-10">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="mdp">
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-lg font-bold mb-2" htmlFor="mdp">
                         Mot de passe
                     </label>
-                    <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="mdp" type="text" placeholder="Mot de passe" />
+                    <input className="appearance-none border rounded w-full py-3 px-4 text-lg text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="mdp" type="password" placeholder="Mot de passe" />
                 </div>
-                <div className="flex items-center justify-between flex-col">
-                    <button className="bg-gray-900 hover:bg-gray-950 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-8" type="button">
+                <div className="flex flex-col items-center">
+                    <button className="bg-gray-900 hover:bg-gray-950 text-white font-bold py-3 px-36 rounded focus:outline-none focus:shadow-outline mb-8" type="button">
                         Connexion
                     </button>
-                    <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                    <a className="inline-block align-baseline font-bold text-lg text-blue-500 hover:text-blue-800" href="#">
                         Mot de passe oublié ?
                     </a>
                 </div>
