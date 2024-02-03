1409c546f2f118a106b8a2a4d4f754089316241b
NicolasDrp <drapier.nicolas07@gmail.com>
2024-02-03 17:14:55 +0100

    fix(bdd) : erreur dans les references

--- react-java-mysql/backend/src/main/resources/data.sql
+++ react-java-mysql/backend/src/main/resources/data.sql
@@ -3,8 +3,8 @@ INSERT INTO utilisateur (prenom_utilisateur, nom_utilisateur, mot_de_passe, emai
 VALUES ('prenom', 'nom', 'motdepasse123', 'mail@mail.com');
 
 -- ajoute de modele de voiture
-INSERT INTO modele (nom_modele, portes, prix_modele) VALUES ('KLASIC', 4, 30000);
+INSERT INTO modele (nom_modele, description_modele, portes, prix_modele) VALUES ('KLASIC','Elle est vieille mais elle roule (Je crois).', 4, 30000);
 
-INSERT INTO modele (nom_modele, portes, prix_modele) VALUES ('LOTRE', 5, 40000);
+INSERT INTO modele (nom_modele, description_modele, portes, prix_modele) VALUES ('LOTRE',"Je sais plus, j'ai plus d'idées", 5, 40000);
 
-INSERT INTO modele (nom_modele, portes, prix_modele) VALUES ('ENKOR1OTRE', 2, 25000);
+INSERT INTO modele (nom_modele, description_modele, portes, prix_modele) VALUES ('ENKOR1OTRE','Encore une autre voiture, bref achète', 2, 25000);
--- react-java-mysql/backend/src/main/resources/schema.sql
+++ react-java-mysql/backend/src/main/resources/schema.sql
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS voiture (
     id_voiture INT AUTO_INCREMENT PRIMARY KEY,
     couleur_voiture VARCHAR(20) NOT NULL,
     id_modele INT NOT NULL,
-    FOREIGN KEY (id_modele) REFERENCES Modele(id_modele)
+    FOREIGN KEY (id_modele) REFERENCES modele(id_modele)
 );
 
 -- Création de la table `commande`
@@ -43,9 +43,9 @@ CREATE TABLE IF NOT EXISTS commande (
     id_voiture INT NOT NULL ,
     id_utilisateur INT NOT NULL,
     id_concessionnaire INT NOT NULL,
-    FOREIGN KEY (id_voiture) REFERENCES Voiture(id_voiture),
-    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id_utilisateur),
-    FOREIGN KEY (id_concessionnaire) REFERENCES Concessionnaire(id_concessionnaire)
+    FOREIGN KEY (id_voiture) REFERENCES voiture(id_voiture),
+    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur),
+    FOREIGN KEY (id_concessionnaire) REFERENCES concessionnaire(id_concessionnaire)
 );
 
 -- Création de la table `notification`
@@ -54,5 +54,5 @@ CREATE TABLE IF NOT EXISTS notification (
     message TEXT NOT NULL,
     date_envoie DATE NOT NULL,
     id_commande INT NOT NULL ,
-    FOREIGN KEY (id_commande) REFERENCES Commande(id_commande)
+    FOREIGN KEY (id_commande) REFERENCES commande(id_commande)
 );
